942b8f894c6d7b17f558d67808b04b2391c0b6aa
JoDeyme <JoDeyme@users.noreply.github.com>
2022-11-24 08:15:47 +0100

    ajout des fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Task;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,21 +11,22 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for(i=0; i<10; i++){
+        for ($i = 0; $i < 10; $i++) {
             $user = new User();
             $user->setUsername('User' . $i);
             $user->setPassword('user' . $i);
             $user->setEmail('user' . $i . '@mail.com');
             $user->setRoles(['ROLE_USER']);
+
+            $task = new Task();
+            $task->setTitle('Task' . $i);
+            $task->setContent('Content' . $i);
+            $task->setCreatedAt(new \DateTime());
+            $task->setIsDone(false);
+            $task->setUser($user);
+
             $manager->persist($user);
         }
-
-        $admin = new User();
-        $admin->setUsername('Admin');
-        $admin->setPassword('admin');
-        $admin->setEmail('admin@mail.com');
-        $admin->setRoles(['ROLE_ADMIN']);
-        $manager->persist($admin);
 
         $manager->flush();
     }
